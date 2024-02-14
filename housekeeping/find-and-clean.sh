e01c45edfe6adb7577716ca064bab34c59272027
laimis9133 <58551069+laimis9133@users.noreply.github.com>
2024-02-15 00:54:12 +0200

    Create find-and-clean.sh

---
+++ housekeeping/find-and-clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Requires MinIO client
+
+while [[ ! -s "/tmp/config.json" ]]; do sleep 2; done  # Not needed if ceph-getkeys used as initContainer in Kubernetes
+mv /tmp/config.json /root/.mc/config.json
+mc find s3/${ceph_bucket}/${dir} --name "*backup*" --older-than ${days} --exec "mc rm {}"
