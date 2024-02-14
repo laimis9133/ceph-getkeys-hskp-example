e7a9a6f7299beae551621c2d8a8c17e6ccc11f20
laimis9133 <58551069+laimis9133@users.noreply.github.com>
2024-02-15 00:56:22 +0200

    Update find-and-clean.sh

--- housekeeping/find-and-clean.sh
+++ housekeeping/find-and-clean.sh
@@ -3,4 +3,4 @@
 
 while [[ ! -s "/tmp/config.json" ]]; do sleep 2; done  # Not needed if ceph-getkeys used as initContainer in Kubernetes
 mv /tmp/config.json /root/.mc/config.json
-mc find s3/${ceph_bucket}/${dir} --name "*backup*" --older-than ${days} --exec "mc rm {}"
+mc find s3/${ceph_bucket}/${dir} --name "*backup*" --older-than ${days} --exec "mc rm {}" >> /logging/log
